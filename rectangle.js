9ed4389ca7bdaa7af1814d6dfe860ce90752ffa1
liwei2016011717 <2335644296@qq.com>
2019-03-27 15:11:08 +0800

    feat-validation

--- rectangle.js
+++ rectangle.js
@@ -4,7 +4,26 @@ $(function(){
       $height=$('#height'),
       $btnCal=$('#btn'),
       $perimeter=$('#perimeter'),
+      $widthValidation=$('#width-validation'),
+      $heightValidation=$('height-validation'),
       $area=$('#area');
+  
+  //校验
+  $height.focusout(function(){
+    var h=$height.val();
+    if(h===''){
+      $heightValidation.html('高度不能为空!');
+      $height.select();
+    }
+  });
+
+  $width.focusout(function(){
+    var w=$width.val();
+    if(w===''){
+      $widthValidation.html('宽度不能为空！');
+      $width.select();
+    }
+  })
   $btnCal.click(function(){
     var w=Number($width.val()),
         h=Number($height.val());
