2424c4dd23d2b7a618350f36106185f334845d84
liwei2016011717 <2335644296@qq.com>
2019-04-02 15:27:56 +0800

    feat-h5-validation

--- rectangle.js
+++ rectangle.js
@@ -2,62 +2,12 @@ $(function(){
   //console.log('hello');
   var $width=$('#width'),
       $height=$('#height'),
-      $btnCal=$('#btn'),
+      $form=$('form'),
       $perimeter=$('#perimeter'),
-      $widthValidation=$('#width-validation'),
-      $heightValidation=$('#height-validation'),
       $area=$('#area');
-  
-  //校验
-  $height.focusout(function(){
-    var h=$height.val();
-    if(h===''){
-      $heightValidation.html('高度不能为空!');
-      $height.select();
-    }else{
-
-      if(Number(h)<0){
-        $heightValidation.html('高度不能为负');
-        $height.select();
-        return;
-      }else{
-        $heightValidation.html('');
-      }
-      if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(h)){
-        $heightValidation.html('必须为合法数字');
-        $height.select();
-        return;
-      }else{
-        $heightValidation.html('');
-      }
-    }
-  });
-
-  $width.focusout(function(){
-    var w=$width.val();
-    if(w===''){
-      $widthValidation.html('宽度不能为空！');
-      $width.select();
-    }else{
-
-      if(Number(w)<0){
-        $widthValidation.html('宽度不能为负');
-        $width.select();
-        return;
-      }else{
-        $widthValidation.html('');
-      }
-
-      if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(w)){
-        $widthValidation.html('必须为合法数字');
-        $width.select();
-        return;
-      }else{
-        $widthValidation.html('');
-      }
-    }
-  });
-  $btnCal.click(function(){
+      
+  $form.submit(function(e){
+    e.preventDefault();//阻止submit后页面刷新
     var w=Number($width.val()),
         h=Number($height.val());
 
